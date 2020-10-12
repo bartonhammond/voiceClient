ac53d0fa58fb4a43bec20951c5b521baf7fb48a6
Barton Hammond <bartonhammond@gmail.com>
2020-10-12 11:24:07 -0500

    properties for android build

---
+++ gradle.properties.sh
@@ -0,0 +1,4 @@
+export FCI_KEYSTORE_PATH=/Users/bartonhammond/Documents/MyFamilyVoice/android/myfamilyvoice.jks
+export FCI_KEYSTORE_PASSWORD=Parkl1fe!
+export FCI_KEY_ALIAS=mfv
+export FCI_KEY_PASSWORD=Parkl1fe!
